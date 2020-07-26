e9216bd38a1aa782f4dd2cd1511110c679b8f06c
Eddy <liang.741@buckeyemail.osu.edu>
2020-07-26 02:24:04 -0400

    added person practice file eddy.dart

---
+++ lib/eddy.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hey bitch"),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text("Hello Eddy"),
+          ),
+      )
+    )
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
